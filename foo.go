0888bdd26f226aece00f3a7243f17d8241f8a3af
Bill Xiong <haibxz@gmail.com>
2017-10-10 14:58:02 +0800

    add some comments

--- foo.go
+++ foo.go
@@ -8,11 +8,13 @@ import (
 
 const VERSION = "1.0.1"
 
+// Foo is a name, just for fun.
 type Foo struct {}
 func (f *Foo) Bar() {
     log.Println("bar!", VERSION)
 }
 
+// NewFoo returns an instance of Foo.
 func NewFoo() *Foo {
     return &Foo{}
 }
